bc97a5f23f64f66e39213ce629e17fd45b0d18cd
Manu <manu.aiumi@gmail.com>
2018-06-16 15:35:23 +0200

    Argumenty - umazano

--- src/cz/manumi/Main.java
+++ src/cz/manumi/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Startuji naši Zoo...");
 
-
-        System.out.println(args[0]);
         System.out.println(args[2]);
         System.out.println(args[1]);
 
