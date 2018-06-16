1bc9ace8549a42862de24f71d52c540f2299d5d9
Manu <manu.aiumi@gmail.com>
2018-06-16 15:33:52 +0200

    Argumenty

--- src/cz/manumi/Main.java
+++ src/cz/manumi/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Startuji naši Zoo...");
 
+
+        System.out.println(args[0]);
+        System.out.println(args[2]);
+        System.out.println(args[1]);
+
     }
 }
